0e0d248ff3fbfeeaa60e776e38b5ca9ba482c32f
CoderWharf <139237018+CoderWharf@users.noreply.github.com>
2023-08-24 17:22:58 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,167 +1,127 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <array>
-#include <algorithm>
-#include <regex>
-
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <stdexcept>
 #ifdef _WIN32
-#define NOMINMAX
-#include <Windows.h>
-#include <wincrypt.h>
-#pragma comment(lib, "crypt32")
+#include <windows.h>
+#include <winreg.h>
 #endif
+using namespace std;
 
-#ifdef __linux__
-#include <unistd.h>
-#endif
-
-std::string exec(const std::string &cmd)
-{
-    std::array<char, 128> buffer;
-    std::string result;
-#ifdef _WIN32
-    SECURITY_ATTRIBUTES sa;
-    ZeroMemory(&sa, sizeof(sa));
-    sa.nLength = sizeof(sa);
-    sa.bInheritHandle = TRUE;
-    HANDLE hRead, hWrite;
-    if (!CreatePipe(&hRead, &hWrite, &sa, 0))
-        return result;
-
-    STARTUPINFOA si;
-    ZeroMemory(&si, sizeof(si));
-    si.cb = sizeof(si);
-    si.dwFlags = STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
-    si.hStdInput = GetStdHandle(STD_INPUT_HANDLE);
-    si.hStdError = hWrite;
-    si.hStdOutput = hWrite;
-    si.wShowWindow = SW_HIDE;
-
-    PROCESS_INFORMATION pi;
-    ZeroMemory(&pi, sizeof(pi));
-    if (CreateProcessA(
-            NULL,
-            const_cast<char *>(cmd.c_str()),
-            NULL,
-            NULL,
-            TRUE,
-            CREATE_NO_WINDOW,
-            NULL,
-            NULL,
-            &si,
-            &pi))
-    {
-        CloseHandle(hWrite);
-
-        DWORD bytesRead;
-        while (ReadFile(hRead, buffer.data(), static_cast<DWORD>(buffer.size()), &bytesRead, NULL))
-        {
-            result.append(buffer.data(), bytesRead);
+string __exec__(const string& cmd) {
+    char buffer[128];
+    string result = "";
+    FILE* pipe = popen(cmd.c_str(), "r");
+    if (!pipe) throw runtime_error("popen() failed!");
+    try {
+        while (fgets(buffer, sizeof buffer, pipe) != NULL) {
+            result += buffer;
         }
-        CloseHandle(hRead);
-
-        WaitForSingleObject(pi.hProcess, INFINITE);
-        CloseHandle(pi.hProcess);
-        CloseHandle(pi.hThread);
-
-        return result;
+    } catch (...) {
+        pclose(pipe);
+        throw;
     }
-    else
-    {
-        CloseHandle(hRead);
-        CloseHandle(hWrite);
-        return result;
-    }
-#else
-    std::shared_ptr<FILE> pipe(popen(cmd.c_str(), "r"), pclose);
-    if (!pipe)
-        return result;
-    while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) != nullptr)
-    {
-        result.append(buffer.data());
-    }
+    pclose(pipe);
     return result;
-#endif
 }
 
-std::string read(const std::string &path)
-{
-    std::ifstream f(path);
-    if (f.is_open())
-    {
-        std::string content((std::istreambuf_iterator<char>(f)), (std::istreambuf_iterator<char>()));
+string __read__(const string& path) {
+    ifstream file(path);
+    if (file.is_open()) {
+        string content((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+        file.close();
         return content;
+    } else {
+        return "";
     }
-    return "";
 }
 
-std::string reg(const std::string &registry, const std::string &key)
-{
 #ifdef _WIN32
+string __reg__(const string& registry, const string& key) {
     HKEY hKey;
-    if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, registry.c_str(), 0, KEY_READ, &hKey) == ERROR_SUCCESS)
-    {
-        DWORD type, cbData = 1024;
-        std::string value(cbData, '\0');
-        if (RegQueryValueExA(hKey, key.c_str(), NULL, &type, reinterpret_cast<BYTE *>(&value[0]), &cbData) == ERROR_SUCCESS)
-        {
-            value.resize(cbData);
-            value.shrink_to_fit();
-            RegCloseKey(hKey);
-            return value;
+    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, registry.c_str(), 0, KEY_READ, &hKey) != ERROR_SUCCESS)
+        throw runtime_error("RegOpenKeyEx() failed!");
+    DWORD dwType, dwSize;
+    if (RegQueryValueEx(hKey, key.c_str(), NULL, &dwType, NULL, &dwSize) != ERROR_SUCCESS)
+        throw runtime_error("RegQueryValueEx() failed!");
+    char* value = new char[dwSize];
+    if (RegQueryValueEx(hKey, key.c_str(), NULL, &dwType, (LPBYTE)value, &dwSize) != ERROR_SUCCESS)
+        throw runtime_error("RegQueryValueEx() failed!");
+    RegCloseKey(hKey);
+    string result(value);
+    delete[] value;
+    return result;
+}
+#endif
+
+string id(bool winregistry = true) {
+#ifdef _WIN32
+    string platform = "win32";
+#else
+    string platform = __exec__("uname -s");
+#endif
+    string id = "";
+    if (platform == "Darwin") {
+        id = __exec__(
+            "ioreg -d2 -c IOPlatformExpertDevice | awk -F\\\" '/IOPlatformUUID/{print $(NF-1)}'"
+        );
+    } else if (platform == "win32" || platform == "cygwin" || platform == "msys") {
+        if (winregistry) {
+            id = __reg__(
+                "SOFTWARE\\Microsoft\\Cryptography", "MachineGuid"
+            );
+        } else {
+            id = __exec__(
+                "powershell.exe -ExecutionPolicy bypass -command (Get-CimInstance -Class Win32_ComputerSystemProduct).UUID"
+            );
         }
-        RegCloseKey(hKey);
+        if (id.empty()) {
+            id = __exec__("wmic csproduct get uuid").substr(39);
+        }
+    } else if (platform.find("Linux") != string::npos) {
+        id = __read__("/var/lib/dbus/machine-id");
+        if (id.empty()) {
+            id = __read__("/etc/machine-id");
+        }
+        if (id.empty()) {
+            string cgroup = __read__("/proc/self/cgroup");
+            if (!cgroup.empty()) {
+                if (cgroup.find("docker") != string::npos) {
+                    id = __exec__("head -1 /proc/self/cgroup | cut -d/ -f3");
+                }
+            }
+        }
+        if (id.empty()) {
+            string mountinfo = __read__("/proc/self/mountinfo");
+            if (!mountinfo.empty()) {
+                if (mountinfo.find("docker") != string::npos) {
+                    id = __exec__("grep 'systemd' /proc/self/mountinfo | cut -d/ -f3");
+                }
+            }
+        }
+        if (id.empty()) {
+            if (__exec__("uname -r").find("microsoft") != string::npos) { // wsl
+                id = __exec__(
+                    "powershell.exe -ExecutionPolicy bypass -command '(Get-CimInstance -Class Win32_ComputerSystemProduct).UUID'"
+                );
+            }
+        }
+    } else if (platform.find("OpenBSD") != string::npos || platform.find("FreeBSD") != string::npos) {
+        id = __read__("/etc/hostid");
+        if (id.empty()) {
+            id = __exec__("kenv -q smbios.system.uuid");
+        }
     }
-    return "";
-#else
-    return "";
-#endif
-}
 
-std::string id(bool winregistry = true)
-{
-#ifdef __APPLE__
-    return exec("ioreg -d2 -c IOPlatformExpertDevice | awk -F\\\" '/IOPlatformUUID/{print $(NF-1)}'");
-#elif defined(_WIN32)
-    if (winregistry)
-        return reg("SOFTWARE\\Microsoft\\Cryptography", "MachineGuid");
-    else
-        return exec("powershell.exe -ExecutionPolicy bypass -command (Get-CimInstance -Class Win32_ComputerSystemProduct^).UUID");
-#elif defined(__linux__)
-    std::string id = read("/var/lib/dbus/machine-id");
-    if (id.empty())
-        id = read("/etc/machine-id");
-    if (id.empty())
-    {
-        std::string cgroup = read("/proc/self/cgroup");
-        if (cgroup.find("docker") != std::string::npos)
-            id = exec("head -1 /proc/self/cgroup | cut -d/ -f3");
+    if (id.empty()) {
+        throw runtime_error("failed to obtain id on platform " + platform);
     }
-    if (id.empty())
-    {
-        std::string mountinfo = read("/proc/self/mountinfo");
-        if (mountinfo.find("docker") != std::string::npos)
-            id = exec("grep 'systemd' /proc/self/mountinfo | cut -d/ -f3");
-    }
-    if (id.empty() && std::regex_search(std::string(uname().release), std::regex(".*microsoft.*")))
-    {
-        id = exec("powershell.exe -ExecutionPolicy bypass -command \"(Get-CimInstance -Class Win32_ComputerSystemProduct^).UUID\"");
-    }
+
     return id;
-#elif defined(__OpenBSD__) || defined(__FreeBSD__)
-    std::string id = read("/etc/hostid");
-    if (id.empty())
-        id = exec("kenv -q smbios.system.uuid");
-    return id;
-#else
-    return "";
-#endif
 }
-
-int main()
-{
-    std::cout << id() << std::endl;
-    return 0;
+int main(){
+    cout << id() << endl;
 }
-
