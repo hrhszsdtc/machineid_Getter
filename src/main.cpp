7cd5956f5ae170ac7f58e4d5d08e6f631bd99720
hrh233 <77053088+hrh233@users.noreply.github.com>
2023-08-25 21:20:15 +0800

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,6 @@
 #include <fstream>
 #include <string>
 #include <sstream>
-#include <cstring>
 #include <algorithm>
 #include <cstdio>
 #include <cstdlib>
@@ -12,17 +11,19 @@
 #include <unistd.h>
 #include <sys/utsname.h>
 #include <sys/mount.h>
+
 using namespace std;
 
 string exec(string cmd)
 {
+    char buffer[128];
+    string result = "";
     FILE *pipe = popen(cmd.c_str(), "r");
     if (!pipe)
     {
         return "ERROR";
     }
-    char buffer[128];
-    string result = "";
+
     while (!feof(pipe))
     {
         if (fgets(buffer, 128, pipe) != NULL)
@@ -37,13 +38,14 @@ string exec(string cmd)
 
 string read_file(string path)
 {
+    string content = "";
     ifstream file(path);
-    if (!file.is_open())
+    if (file.is_open())
     {
-        return "";
+        content = string((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+        file.close();
     }
-    string content((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
-    file.close();
+
     return content;
 }
 
@@ -60,24 +62,25 @@ string reg_read(string key)
     return "";
 }
 
-string id(bool winregistry = true)
+string get_machine_id()
 {
+    string machine_id = "";
+
     struct utsname buf;
     uname(&buf);
 
     if (strcmp(buf.sysname, "Darwin") == 0)
     {
-        string cmd = "ioreg -d2 -c IOPlatformExpertDevice | awk -F\\\" '/IOPlatformUUID/{print $(NF-1)}'";
-        return exec(cmd);
+        machine_id = exec("ioreg -d2 -c IOPlatformExpertDevice | awk -F\\\" '/IOPlatformUUID/{print $(NF-1)}'");
     }
     else if (strcmp(buf.sysname, "Linux") == 0)
     {
-        string u_id = read_file("/var/lib/dbus/machine-id");
-        if (u_id.empty())
+        machine_id = read_file("/var/lib/dbus/machine-id");
+        if (machine_id.empty())
         {
-            u_id = read_file("/etc/machine-id");
+            machine_id = read_file("/etc/machine-id");
         }
-        if (u_id.empty())
+        if (machine_id.empty())
         {
             string cgroup = read_file("/proc/self/cgroup");
             if (cgroup.find("docker") != string::npos)
@@ -85,10 +88,10 @@ string id(bool winregistry = true)
                 regex pattern("[0-9,a-z]{64}");
                 smatch match;
                 regex_search(cgroup, match, pattern);
-                u_id = match.str(0);
+                machine_id = match.str(0);
             }
         }
-        if (u_id.empty())
+        if (machine_id.empty())
         {
             string mountinfo = read_file("/proc/self/mountinfo");
             if (mountinfo.find("docker") != string::npos)
@@ -105,57 +108,48 @@ string id(bool winregistry = true)
                 {
                     paths.push_back(token);
                 }
-                u_id = paths.back();
+                machine_id = paths.back();
             }
         }
-        if (u_id.empty() && strstr(buf.release, "microsoft"))
+        if (machine_id.empty() && strstr(buf.release, "microsoft"))
         {
-            string cmd = "powershell.exe -ExecutionPolicy bypass -command (Get-CimInstance -Class Win32_ComputerSystemProduct).UUID";
-            return exec(cmd);
+            machine_id = exec("powershell.exe -ExecutionPolicy bypass -command (Get-CimInstance -Class Win32_ComputerSystemProduct).UUID");
         }
-        return u_id;
     }
     else if (strcmp(buf.sysname, "FreeBSD") == 0 || strcmp(buf.sysname, "OpenBSD") == 0)
     {
-        string u_id = read_file("/etc/hostid");
-        if (u_id.empty())
+        machine_id = read_file("/etc/hostid");
+        if (machine_id.empty())
         {
-            string cmd = "kenv -q smbios.system.uuid";
-            return exec(cmd);
+            machine_id = exec("kenv -q smbios.system.uuid");
         }
-        return u_id;
     }
     else if (strcmp(buf.sysname, "Windows") == 0)
     {
-        if (winregistry)
+        machine_id = reg_read("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Cryptography");
+        if (machine_id.empty())
         {
-            return reg_read("HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Cryptography");
-        }
-        else
-        {
-            string cmd = "powershell.exe -ExecutionPolicy bypass -command (Get-CimInstance -Class Win32_ComputerSystemProduct).UUID";
-            string output = exec(cmd);
-            if (output.empty())
+            machine_id = exec("powershell.exe -ExecutionPolicy bypass -command (Get-CimInstance -Class Win32_ComputerSystemProduct).UUID");
+            if (machine_id.empty())
             {
-                cmd = "wmic csproduct get uuid";
-                output = exec(cmd);
-                if (!output.empty())
+                machine_id = exec("wmic csproduct get uuid");
+                if (!machine_id.empty())
                 {
-                    output = output.substr(4);
+                    machine_id = machine_id.substr(4);
                 }
             }
-            return output;
         }
     }
     else
     {
         throw "Unsupported platform";
     }
+
+    return machine_id;
 }
 
 int main()
 {
-    cout << id() << endl;
+    cout << get_machine_id() << endl;
     return 0;
 }
-
